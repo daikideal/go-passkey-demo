b77883580e031b01ac33a78023c04595a0da0948
daikideal <54879050+daikideal@users.noreply.github.com>
2023-11-04 11:17:37 +0900

    Add: bunマイグレーションでusersテーブルを追加
    bunの公式サンプルを流用してマイグレーションCLIを追加。
    ユーザーが登録できるテーブルを作成した。
    
    `go run . db **`は /migration ディレクトリで行う必要があり、複数の引数が必要なのでMakefileは役に立たないので削除した。

--- migration/main.go
+++ migration/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	_ "github.com/lib/pq"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/migrate"
+	"github.com/urfave/cli/v2"
 )
 
 // TODO: 環境変数から取得する
@@ -19,17 +26,178 @@ var DSN = fmt.Sprintf(
 )
 
 func main() {
-	db, err := sql.Open("postgres", DSN)
-	defer db.Close()
-
+	// DBに接続
+	sqldb, err := sql.Open("postgres", DSN)
+	defer sqldb.Close()
 	if err != nil {
 		panic(err)
 	}
 
+	db := bun.NewDB(sqldb, pgdialect.New())
+
 	err = db.Ping()
 	if err != nil {
-		fmt.Printf("Connection failed: %v\n", err)
+		panic(err)
 	}
 
-	fmt.Println("Connection established.")
+	// マイグレーションファイルの読み込み
+	//
+	// このファイルがあるディレクトリ内でマイグレーションファイルを探すようになっているので、
+	// マイグレーションファイルを保存するためのディレクトリをもう一段階深く掘っておき、そちらに main.go を作成し、 init() で読み込むようにしてもいいかもしれない。
+	migrations := migrate.NewMigrations()
+	if err = migrations.DiscoverCaller(); err != nil {
+		panic(err)
+	}
+
+	// マイグレーションCLIを起動
+	app := &cli.App{
+		Name: "bun",
+		Commands: []*cli.Command{
+			newDBCommand(migrate.NewMigrator(db, migrations)),
+		},
+	}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// # マイグレーションコマンドの定義
+//
+// [example/migrate/main.go]からコピーした。
+// 
+// マイグレーションファイルはsqlかgoで作成できる。
+// sqlの方が認知負荷は小さそうだが、goの方は同じファイルにupとdownの両方を書くことができるので、
+// マイグレーションファイルが増えても管理しやすいかもしれない。
+//
+// [example/migrate/main.go]: https://github.com/uptrace/bun/blob/master/example/migrate/main.go
+func newDBCommand(migrator *migrate.Migrator) *cli.Command {
+	return &cli.Command{
+		Name:  "db",
+		Usage: "database migration",
+		Subcommands: []*cli.Command{
+			{
+				Name:  "init",
+				Usage: "create migration management tables, `bun_migrations` and `bun_migration_locks`",
+				Action: func(c *cli.Context) error {
+					return migrator.Init(c.Context)
+				},
+			},
+			{
+				Name:  "migrate",
+				Usage: "migrate database",
+				Action: func(c *cli.Context) error {
+					if err := migrator.Lock(c.Context); err != nil {
+						return err
+					}
+					defer migrator.Unlock(c.Context) //nolint:errcheck
+
+					group, err := migrator.Migrate(c.Context)
+					if err != nil {
+						return err
+					}
+					if group.IsZero() {
+						fmt.Printf("there are no new migrations to run (database is up to date)\n")
+						return nil
+					}
+					fmt.Printf("migrated to %s\n", group)
+					return nil
+				},
+			},
+			{
+				Name:  "rollback",
+				Usage: "rollback the last migration group",
+				Action: func(c *cli.Context) error {
+					if err := migrator.Lock(c.Context); err != nil {
+						return err
+					}
+					defer migrator.Unlock(c.Context) //nolint:errcheck
+
+					group, err := migrator.Rollback(c.Context)
+					if err != nil {
+						return err
+					}
+					if group.IsZero() {
+						fmt.Printf("there are no groups to roll back\n")
+						return nil
+					}
+					fmt.Printf("rolled back %s\n", group)
+					return nil
+				},
+			},
+			{
+				Name:  "lock",
+				Usage: "lock migrations",
+				Action: func(c *cli.Context) error {
+					return migrator.Lock(c.Context)
+				},
+			},
+			{
+				Name:  "unlock",
+				Usage: "unlock migrations",
+				Action: func(c *cli.Context) error {
+					return migrator.Unlock(c.Context)
+				},
+			},
+			{
+				Name:  "create_go",
+				Usage: "create Go migration",
+				Action: func(c *cli.Context) error {
+					name := strings.Join(c.Args().Slice(), "_")
+					mf, err := migrator.CreateGoMigration(c.Context, name)
+					if err != nil {
+						return err
+					}
+					fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
+					return nil
+				},
+			},
+			{
+				Name:  "create_sql",
+				Usage: "create up and down SQL migrations",
+				Action: func(c *cli.Context) error {
+					name := strings.Join(c.Args().Slice(), "_")
+					files, err := migrator.CreateSQLMigrations(c.Context, name)
+					if err != nil {
+						return err
+					}
+
+					for _, mf := range files {
+						fmt.Printf("created migration %s (%s)\n", mf.Name, mf.Path)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:  "status",
+				Usage: "print migrations status",
+				Action: func(c *cli.Context) error {
+					ms, err := migrator.MigrationsWithStatus(c.Context)
+					if err != nil {
+						return err
+					}
+					fmt.Printf("migrations: %s\n", ms)
+					fmt.Printf("unapplied migrations: %s\n", ms.Unapplied())
+					fmt.Printf("last migration group: %s\n", ms.LastGroup())
+					return nil
+				},
+			},
+			{
+				Name:  "mark_applied",
+				Usage: "mark migrations as applied without actually running them",
+				Action: func(c *cli.Context) error {
+					group, err := migrator.Migrate(c.Context, migrate.WithNopMigration())
+					if err != nil {
+						return err
+					}
+					if group.IsZero() {
+						fmt.Printf("there are no new migrations to mark as applied\n")
+						return nil
+					}
+					fmt.Printf("marked as applied %s\n", group)
+					return nil
+				},
+			},
+		},
+	}
 }
